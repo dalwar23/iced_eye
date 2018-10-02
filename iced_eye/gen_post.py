994fb1913f61fe9a4a8fbe0b535859019027555d
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-10-02 21:48:22 +0300

    recompile with latest post

--- iced_eye/gen_post.py
+++ iced_eye/gen_post.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 # Import python libraries
 import os
 import sys
+import io
 from datetime import datetime
 from pyrainbowterm import *
 try:
@@ -21,6 +22,10 @@ __email__ = 'dalwar.hossain@protonmail.com'
 templates_dir = '../templates'
 
 
+# Set default encoding
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 # Create a function to generate posts
 def generate_post():
     """
@@ -32,13 +37,15 @@ def generate_post():
     # Read post
     print('Reading post.....', log_type='info')
     html_post_file = os.path.join(templates_dir, 'latest_post.html')
-    with open(html_post_file, 'r') as post:
+    with io.open(html_post_file, 'r', encoding='utf8') as post:
         html_post = post.read()
     # Create a Beautifulsoup object to parse the html
     print('Parsing HTML.....', log_type='info')
     soup = BeautifulSoup(html_post, 'html.parser')
     post_title = soup.title.string
-    post_body = soup.body.string
+    post_body_elements = soup.select('body  p')
+    # Join all the paragraph's of the post body
+    post_body = '\n'.join(str(item) for item in post_body_elements)
     post_category = soup.category.string
     post_date = datetime.now().strftime('%A, %B %d, %Y @ %H:%M')
     # Now read the pre_blog template and create blog.html
@@ -48,8 +55,8 @@ def generate_post():
     print('Creating blog.html file.....', log_type='info')
     replace_dict = {'_post_title_' : post_title, '_post_body_' : post_body,
                     '_post_date_time_' : post_date, '_post_category_' : post_category}
-    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-        for line in  infile:
+    with io.open(input_file, 'r', encoding='utf8') as infile, io.open(output_file, 'w', encoding='utf8') as outfile:
+        for line in infile:
             for source, target in replace_dict.iteritems():
                 line = line.replace(source, target)
             outfile.write(line)
