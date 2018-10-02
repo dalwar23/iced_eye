ad4cbe95e8b56d021d13167101ace7d386460c6d
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-10-02 15:36:06 +0300

    post generator added

---
+++ iced_eye/gen_post.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+# Import python libraries
+import os
+import sys
+from datetime import datetime
+from pyrainbowterm import *
+try:
+    from bs4 import BeautifulSoup
+except ImportError as e:
+    print('Can not import BeautifulSoup', log_type='error', color='red')
+
+# Source code meta data
+__author__ = 'Dalwar Hossain'
+__email__ = 'dalwar.hossain@protonmail.com'
+
+# Templates directory
+templates_dir = '../templates'
+
+
+# Create a function to generate posts
+def generate_post():
+    """
+    This function genrates posts in the blog section of the website
+    It uses text file from posts directory to create posts
+
+    :return: (obj) File oject
+    """
+    # Read post
+    print('Reading post.....', log_type='info')
+    html_post_file = os.path.join(templates_dir, 'latest_post.html')
+    with open(html_post_file, 'r') as post:
+        html_post = post.read()
+    # Create a Beautifulsoup object to parse the html
+    print('Parsing HTML.....', log_type='info')
+    soup = BeautifulSoup(html_post, 'html.parser')
+    post_title = soup.title.string
+    post_body = soup.body.string
+    post_category = soup.category.string
+    post_date = datetime.now().strftime('%A, %B %d, %Y @ %H:%M')
+    # Now read the pre_blog template and create blog.html
+    input_file = os.path.join(templates_dir, 'pre_blog.html')
+    output_file = os.path.join(templates_dir, 'blog.html')
+    # Read input tamplate and create blog.html file
+    print('Creating blog.html file.....', log_type='info')
+    replace_dict = {'_post_title_' : post_title, '_post_body_' : post_body,
+                    '_post_date_time_' : post_date, '_post_category_' : post_category}
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in  infile:
+            for source, target in replace_dict.iteritems():
+                line = line.replace(source, target)
+            outfile.write(line)
+    # HTML file creation complete
+    print('HTML file creation complete!', log_type='info')
+
+
+# Boiler plate to run this program
+if __name__ == '__main__':
+    generate_post()
